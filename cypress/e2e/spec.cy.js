8f8b1918bc09c0b0a03f5a55dff83ad469b17b44
Clay Blanton <dcblanton78@gmail.com>
2023-03-30 12:24:24 -1000

    again4

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 describe('Test My Portfolio', () => {
   it('site is up', () => {
-    cy.visit('http://127.0.0.1:5501/')
+    cy.visit('http://127.0.0.1:3030/')
    
   })
   context("message form", () => {
     it('contact message form works', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       cy.get('[data-cy="contact-name"]').type("Clay Blanton")
       cy.get('[data-cy="contact-email"]').type("clay@clay.com")
@@ -15,7 +15,7 @@ describe('Test My Portfolio', () => {
       cy.get('[data-cy="outcomeMessage"]').contains("Thank you for your message. I'll get back to you as soon as I can!!")
     })
     it('contact message form throws an error when name is empty', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       //cy.get('[data-cy="contact-name"]').type("")
       cy.get('[data-cy="contact-email"]').type("test@clay.com")
@@ -25,7 +25,7 @@ describe('Test My Portfolio', () => {
       cy.get('[data-cy="outcomeMessage"]').contains("Please enter your name!")
     })
     it('contact message form throws an error when email is empty', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       cy.get('[data-cy="contact-name"]').type("Clay Blanton")
       //cy.get('[data-cy="contact-email"]').type("test@clay.com")
@@ -35,7 +35,7 @@ describe('Test My Portfolio', () => {
       cy.get('[data-cy="outcomeMessage"]').contains("Please enter an email address!")
     })
     it('contact message form throws an error when an invalid email address is entered', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       cy.get('[data-cy="contact-name"]').type("Clay")
       cy.get('[data-cy="contact-email"]').type("testclay.com")
@@ -45,7 +45,7 @@ describe('Test My Portfolio', () => {
       cy.get('[data-cy="outcomeMessage"]').contains("Please enter a valid email address!")
     })
     it('contact message form throws an error when subject is empty', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       cy.get('[data-cy="contact-name"]').type("Clay Blanton")
       cy.get('[data-cy="contact-email"]').type("test@clay.com")
@@ -55,7 +55,7 @@ describe('Test My Portfolio', () => {
       cy.get('[data-cy="outcomeMessage"]').contains("Please enter a Subject!")
     })
     it('contact message form throws an error when message body is empty', () => {
-      cy.visit('http://127.0.0.1:5501/')
+      cy.visit('http://127.0.0.1:3030/')
       cy.get('[data-cy="cy-contact"]').click()
       cy.get('[data-cy="contact-name"]').type("Clay Blanton")
       cy.get('[data-cy="contact-email"]').type("test@clay.com")
